27554fab14e95a59bd4a3d41de394e707cbace2c
aimen <aimen.cherif@etu.univ-amu.fr>
2022-10-26 20:42:05 +0200

    begin task

--- src/main/java/model/regulators/BooleanActivator.java
+++ src/main/java/model/regulators/BooleanActivator.java
@@ -1,6 +1,7 @@
 package model.regulators;
 
 public class BooleanActivator extends BooleanRegulator{
+    //TODO: task3 begin with this
     @Override
     public double inputFunction() {
         return 1.;
