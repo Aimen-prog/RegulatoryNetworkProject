cba4b1f92de80828a8945b9c47d22bb8edb34e99
aimen <aimen.cherif@etu.univ-amu.fr>
2022-11-02 16:48:55 +0100

    simulationtest

--- src/test/java/model/events/AbstractSimulationEventTest.java
+++ src/test/java/model/events/AbstractSimulationEventTest.java
@@ -22,13 +22,25 @@ public class AbstractSimulationEventTest {
         RegulatoryGene gene1 = new ConcreteRegulatoryGene("INS",80, 0.9,
                 50, false);
         RegulatoryGene gene2 = new ConcreteRegulatoryGene("P53",20, 0.5,
-                20, false);
+                20, true);
+        gene1.setProteinConcentration(gene1.getInitialProteinConcentration());
+        gene2.setProteinConcentration(gene2.getInitialProteinConcentration());
         geneList.add(gene1);
         geneList.add(gene2);
-
         AbstractSimulationEvent simulation = new SetProteinConcentrationEvent(geneList,10.,15.);
+        assertThat(geneList.get(0).getProteinConcentration()).isEqualTo(50.);
+        assertThat(geneList.get(1).getProteinConcentration()).isEqualTo(20.);
+
         simulation.updateGenes();
+        assertThat(geneList.get(0).getProteinConcentration()).isEqualTo(15.);
         assertThat(geneList.get(1).getProteinConcentration()).isEqualTo(15.);
+
+        AbstractSimulationEvent simulation2 = new SetSignaledEvent(geneList,10.,true);
+        assertFalse(geneList.get(0).isSignaled());
+        assertTrue(geneList.get(1).isSignaled());
+        simulation2.updateGenes();
+        assertTrue(geneList.get(0).isSignaled());
+        assertTrue(geneList.get(1).isSignaled());
     }
 
 }
